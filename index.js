a0474e67c46f9fbe867e7bc7110215090105b498
Kaptard <kilian.volb@protonmail.com>
2017-06-29 22:37:55 +0200

    Add methods for new endpoints

--- index.js
+++ index.js
@@ -19,13 +19,62 @@ class Nexus extends Blitz {
         super(options)
     }
 
+
     /**
      * Get primary stats for specific item
      */
     getItemStats(name) {
         return new Promise((resolve, reject) => {
             this.get(`/warframe/v1/items/${name}/statistics`)
-                .then(res => resolve(JSON.parse(res.body)))
+                .then(res => resolve(res))
+                .catch(err => reject(err))
+        })
+    }
+
+
+    /**
+     * Get item price list
+     */
+    getItemPrices() {
+        return new Promise((resolve, reject) => {
+            this.get("/warframe/v1/items/prices")
+                .then(res => resolve(res))
+                .catch(err => reject(err))
+        })
+    }
+
+
+    /**
+     * Item distribution (supply/demand)
+     */
+    getItemDistribution() {
+        return new Promise((resolve, reject) => {
+            this.get("/warframe/v1/items/distribution")
+                .then(res => resolve(res))
+                .catch(err => reject(err))
+        })
+    }
+
+
+    /**
+     * Status of all connected bots
+     */
+    getBotStatus() {
+        return new Promise((resolve, reject) => {
+            this.get("/warframe/v1/bots/status")
+                .then(res => resolve(res))
+                .catch(err => reject(err))
+        })
+    }
+
+
+    /**
+     * Status of all connected bots
+     */
+    getPlayerProfile(username) {
+        return new Promise((resolve, reject) => {
+            this.get(`/warframe/v1/players/${username}/profile`
+                .then(res => resolve(res))
                 .catch(err => reject(err))
         })
     }
