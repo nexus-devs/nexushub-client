c02765b0974fe96cebaee017ee822cd4e10e4743
Kilian <kilian@nexus-stats.com>
2018-11-07 23:46:49 +0100

    refactor: Switch to default API to production API

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ class Nexus extends Client {
   constructor(options) {
     options = {
       ...{
-        api_url: "wss://api.staging.nexushub.co/ws",
-        auth_url: "wss://auth.staging.nexushub.co/ws"
+        api_url: "wss://api.nexushub.co/ws",
+        auth_url: "wss://auth.nexushub.co/ws"
       },
       ...options
     }
